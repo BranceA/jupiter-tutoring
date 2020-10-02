84486ca79fd0067860282e75238d95d12bc86e49
BranceA <brancealexander@gmail.com>
2020-10-02 12:53:08 -0500

    added heavily annotated version

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -5,7 +5,9 @@ var wantsToPlay = confirm("You wanna play rock, paper, scissors?")
 while (wantsToPlay){
     var someoneWon = false;
     while (!someoneWon){
+        // Asking player for a choice and capturing response in variable
         var playerChoice = prompt("Type rock paper or scissors");
+        // Calls function that returns a random choice between rock paper or scissors and stores the return in a variable
         var computerChoice = getComputerChoice();
         alert("ROCK PAPER SCISSORS!!!");
         alert("Player: " + playerChoice + "    |     Computer: " + computerChoice)
@@ -41,18 +43,66 @@ function getComputerChoice(){
     return randomChoice;
 }
 
-function findWinner(player, computer){
-    if(player === "rock" && computer === "scissors"){
+function findWinner(pChoice, cChoice){
+    if(pChoice === "rock" && cChoice === "scissors"){
         return "YOU WIN!!!";
-    }else if(player === "rock" && computer === "paper"){
+    }else if(pChoice === "rock" && cChoice === "paper"){
         return "YOU LOSE!!!";
-    }else if(player === "paper" && computer === "rock"){
+    }else if(pChoice === "paper" && cChoice === "rock"){
         return "YOU WIN!!!";
-    }else if(player === "paper" && computer === "scissors"){
+    }else if(pChoice === "paper" && cChoice === "scissors"){
         return "YOU LOSE!!!";
-    }else if(player === "scissors" && computer === "paper"){
+    }else if(pChoice === "scissors" && cChoice === "paper"){
         return "YOU WIN!!!";
-    }else if(player === "scissors" && computer === "rock"){
+    }else if(pChoice === "scissors" && cChoice === "rock"){
         return "YOU LOSE";
     }
 }
+
+
+
+// //Intial check to see if the user wants to play. If no then no code will run in the while loop.
+// var wantsToPlay = confirm("You wanna play rock, paper, scissors?")
+//
+// // This loop will continue to run as long as the player keeps wanting to play.
+// // There is a confirm at the bottom of the loop that asks the player if they want to continue after every game.
+// while (wantsToPlay){
+//     // The inner loop will continue to run until there is a winner.
+//     // I want the computer and the player to keep playing over and over until there is no tie.
+//     // I set the variable to false before the loop runs for the first time because there hasn't even been 1 round yet so there can't be a winner.
+//     var someoneWon = false;
+//
+//     // Inner loop to keep the game going until there is no tie.
+//     while (!someoneWon){
+//         // Asking player for a choice and capturing response in variable
+//         var playerChoice = prompt("Type rock paper or scissors");
+//         // Calls function that returns a random string that's either "rock" "paper" or "scissors" and stores the return in a variable
+//         var computerChoice = getComputerChoice();
+//         // Alert to build suspense.
+//         alert("ROCK PAPER SCISSORS!!!");
+//         // This alert displays the players choice by concatenating the player and computer choice into a string.
+//         alert("Player: " + playerChoice + "    |     Computer: " + computerChoice)
+//         // If there's a tie then the alert informs the player that it's a tie AND NOTHING ELSE!!!
+//         // The code never looks at the else statement so we end up at the top of the loop since someoneWon is still false
+//         if(playerChoice.toLowerCase() === computerChoice){
+//             alert("It's a tie!!! Get ready to play again.");
+//         }
+//         // If there is no tie then there is a winner. Hooray!
+//         else {
+//             // Here the findWinner function is called by passing the player's and computer's choice as arguments.
+//             // The function will return a string of either "YOU WIN!!!" or "YOU LOSE!!!" and we store it in a variable.
+//             var winnerMessage = findWinner(playerChoice, computerChoice);
+//             // This alert displays the winner message. This was done on 2 lines to read the code easier.
+//             alert(winnerMessage);
+//             // Since there was no tie we reassign the someoneWon variable to true.
+//             // We do this so we don't loop through another round of play. The game is over. Someone won.
+//             someoneWon = true;
+//         }
+//     }
+//     // This is outside the inner while loop so it will only run after a winner is declared.
+//     // Whether the player hits confirm for true or cancel for false will determine if we start all over with a new game.
+//     wantsToPlay = confirm("Do you want to play again?");
+// }
+//
+// // This is outside both loops. This will only run if the player chooses not to play or not play again.
+// alert("See you later.")
